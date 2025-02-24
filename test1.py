38b5f608cdbcd56466282b3a5229481e3cf58589
PTK-A240386-MB <ptk-a240386-mb@PTK-A240386-MBui-MacBookPro.local>
2025-02-24 17:44:09 +0900

    add csv file reader

--- test1.py
+++ test1.py
@@ -1,20 +1,26 @@
 import pandas as pd
 import numpy as np
+import csv
 
-wdbc_file=pd.read_csv("wdbc.csv")
 
-print(wdbc_file.iloc[0])
+# open & read csv file
+reader = csv.reader(open('wdbc.csv', mode='r', newline=''))
+
+# first column 
+for row in reader:
+    print(row[0])
+
+
 # normalize the dataset 
 # normalize = (x-mean)/std
-count=0
-for i in range(len(wdbc_file.columns)):
-    data=np.array(wdbc_file.iloc[i])
-    print("data")
-    print(data)
-    print("/")
-    mean=np.mean(data)
-    std=np.std(data)
-    normalized_datas=(data-mean)/std
-    print(normalized_datas)
-    count+=1
-    print(count)
+# for i in range(len(wdbc_file.columns)):
+#     data=np.array(wdbc_file.iloc[i])
+#     print("data")
+#     print(data)
+#     print("/")
+#     mean=np.mean(data)
+#     std=np.std(data)
+#     normalized_datas=(data-mean)/std
+#     print(normalized_datas)
+#     count+=1
+#     print(count)
