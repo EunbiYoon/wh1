53bc3d9ac97ff2ead2c5db53970ac041082e0ad9
PTK-A240386-MB <ptk-a240386-mb@PTK-A240386-MBui-MacBookPro.local>
2025-02-25 14:23:15 +0900

    shuffle and split data using sklearn

--- test1.py
+++ test1.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import numpy as np
-import openpyxl
+import sklearn as sk
 
 # read CSV file
 wdbc_file = pd.read_csv('wdbc.csv', header=None)
-wdbc_file.to_excel("original.xlsx")
 
-# Convert DataFrame to NumPy array
+# convert DataFrame to NumPy array
 wdbc_numpy = wdbc_file.to_numpy()
 
-# Min-max normalization using NumPy
+# min-max normalization using NumPy
+# for last column, min = 0 & max =1 -> value is just itself
 normalized_numpy = (wdbc_numpy - np.min(wdbc_numpy, axis=0)) / (np.max(wdbc_numpy, axis=0) - np.min(wdbc_numpy, axis=0))
 
 # change normalized data to pandas dataframe
-normalized_df = pd.DataFrame(normalized_numpy, columns=wdbc_file.columns)
+normalized_data = pd.DataFrame(normalized_numpy, columns=wdbc_file.columns)
+normalized_data.to_excel("normalized_df.xlsx")
 
-# end of column is follow with original one
-start_data = pd.concat([normalized_df, pd.DataFrame([wdbc_file.iloc[-1]], columns=wdbc_file.columns)], ignore_index=True)
+# shuffle the DataFrame
+shuffled_data = sk.utils.shuffle(normalized_data)
 
-start_data.to_excel("new.xlsx")
+# split data with training and testing
+train_data, test_data= sk.model_selection.train_test_split(shuffled_data, test_size=0.2)
