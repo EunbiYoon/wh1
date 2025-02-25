3d831c792ce00bff87e7b238b269c339ff2ee817
Software Developer <yinyinbei0717@gmail.com>
2025-02-26 00:16:16 +0900

    insert accuracy and graph

--- test1.py
+++ test1.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import sklearn as sk
-import matplotlib
+import matplotlib.pyplot as plt
+
+def random_state=42
 
 # Prepared data before KNN
 def process_dataset():
@@ -19,11 +21,12 @@ def process_dataset():
     normalized_data = pd.DataFrame(normalized_numpy, columns=wdbc_file.columns)
 
     # shuffle the DataFrame
-    ##### 나중에 지우기 랜덤하게 하지 않게 하기 위해서 고정함
-    shuffled_data = sk.utils.shuffle(normalized_data, random_state=42)
+    ##### 나중에 지우기 랜덤하게 하지 않게 하기 위해서 고정함 random_state=42
+    shuffled_data = sk.utils.shuffle(normalized_data)
 
     # split data with training and testing
-    train_data, test_data= sk.model_selection.train_test_split(shuffled_data, test_size=0.2, random_state=42)
+    ##### 나중에 지우기 랜덤하게 하지 않게 하기 위해서 고정함 random_state=42
+    train_data, test_data= sk.model_selection.train_test_split(shuffled_data, test_size=0.2)
 
     # reset index both train_data and test_data
     train_data=train_data.reset_index(drop=True)
@@ -33,11 +36,15 @@ def process_dataset():
     train_data.to_excel('train_data.xlsx')
     test_data.to_excel('test_data.xlsx')
 
+    # message
+    print("Normalized and Shuffling are done. Split train_data and test_data. Please wait...")
+
     # return final train_data, test_data
     return train_data, test_data
 
 # caculate Euclidean Distance
 def euclidean_formula(x1,x2):
+    # follow eucliean distance formula
     euclidean_distance = np.sqrt(np.sum(x1-x2)**2)
     return euclidean_distance
 
@@ -54,12 +61,18 @@ def majority_formula(list):
 
 # Calculate Euclidean Distane in train_data
 def euclidean_matrix(train_data):
+    # make numpy matrix
     train_euclidean=np.zeros((len(train_data),len(train_data)))
+
+    # apply euclidean formula
     for i in range(len(train_data)):
         for j in range(len(train_data)):
             train_euclidean[i,j]=euclidean_formula(train_data.iloc[i], train_data.iloc[j])
     train_euclidean=pd.DataFrame(train_euclidean)
-    train_euclidean.to_excel("train_eculidean.xlsx")
+    train_euclidean.to_excel("train_euclidean.xlsx")
+
+    # message
+    print("Euclidean matrix has been created")
     return train_euclidean
 
 # Insert predicted list in the original dataset at the last column
@@ -67,8 +80,24 @@ def insert_predicted(predicted_list, train_data):
     train_data.loc[-1]=predicted_list
     return train_data
 
+# Accuracy in Training and Testing
+def calculate_accuracy(actual_list, predicted_list, train_data):
+    # compare two column. matched->1, mismatched->0
+    compared_result = list((actual_list == predicted_list).astype(int))
+    
+    # calculate accuracy
+    accuracy=(sum(compared_result)/len(compared_result))   
+
+    # return accuracy value
+    return accuracy
+
+                                        
 # KNN algorithm using Euclidian Matrix
 def knn_algorithm(k, train_euclidean, train_data):
+    # make empty dataframe
+    knn_trained_data=pd.DataFrame(train_data)
+    accuracy_table=pd.DataFrame()
+
     # Iterate over k values : 1~51 odd number
     for i in range(1,k+1,2): 
         # j is for data instances 
@@ -86,34 +115,48 @@ def knn_algorithm(k, train_euclidean, train_data):
             train_smallest=train_data.loc[smallest_index_cutoff]
 
             # get the last column from smallest_index_cutoff
-            train_last=train_smallest[30]
+            train_last=train_smallest[len(train_data.columns)-1]
 
             # check majority 
             final_predicted_value=majority_formula(train_last)
 
             # merge final_predicted_value to train_data
-            train_data.at[j,"k="+str(i)]=final_predicted_value
+            knn_trained_data.at[j,"k="+str(i)]=final_predicted_value
 
-            print(f"make knn algorithm : k={i} , train_instance={j}")
+            # message
+            print(f"knn algorithm : k={i} , train_instance={j}")
+        
+        # calcualte accuracy depending on k and make accuracy_table
+        accuracy_value=calculate_accuracy(knn_trained_data[len(train_data.columns)-1], knn_trained_data["k="+str(i)], knn_trained_data)
+        accuracy_table.at[i,"accuracy"]=accuracy_value
 
-    #Save the DataFrame once after all iterations
-    train_data.to_excel("merge_k.xlsx")
-    print("Saved to merge_k.xlsx")
-
-# Accuracy in Training and Testing
-def accuracy_matrix():
-    ret
+    # Save the DataFrame once after all iterations
+    knn_trained_data.to_excel("knn_algorithm.xlsx")
+    accuracy_table.to_excel("accuracy_table.xlsx")
+    return accuracy_table 
 
 # Graph created with k
-def graph_knn():
-    return 0
+def graph_accuracy(accuracy_table, title):
+    # plot the data
+    plt.figure(figsize=(10, 5))
+    plt.plot(accuracy_table)
+
+    # formatting 
+    plt.xlabel("Value of k")
+    plt.ylabel("Accuracy over "+title)
+    plt.savefig(title+".png",dpi=300, bbox_inches='tight')
+
+    # message
+    "save graph image file to the folder"
 
 # main function - united all function ahead
 def main():
     train_data, test_data=process_dataset()
     train_euclidean=euclidean_matrix(train_data)
-    knn_algorithm(51, train_euclidean, train_data)
-
+    accuracy_table=knn_algorithm(51, train_euclidean, train_data)
+    graph_accuracy(accuracy_table, "training data")
+    # message
+    "hw1 is done"
 
 # ensures that the main function is executed only
 if __name__ == "__main__":
